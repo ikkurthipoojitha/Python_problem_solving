a3c17dc3ed6a74a45dabd8fb4eb2867aa494a589
ikkurthipoojitha <ipoojitha1997@gmail.com>
2019-04-17 19:23:37 +0530

    Update Maxofthree.py

--- Maxofthree.py
+++ Maxofthree.py
@@ -1,16 +1,9 @@
 a=int(input("enter the value"))
 b=int(input("enter the value"))
 c=int(input("enter the value"))
-if a>b:
-    if a>c:
-        print(a,"is maximum")
-    elif b>c:
-        print(b, "is maximum")
-    else:
-        print(c,"is maximum")
-
-
-elif b>c:
+if a>b and a>c:
+    print(a,"is maximum")
+elif b>c and b>a:
     print(b,"is maximum")
 else:
     print(c,"is maximum")
